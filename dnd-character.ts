30ffa6ff9005910f55d8ff7d34b34f80e46739b6
egrao-innovation <em.grao@innovation.es>
2022-03-30 11:04:10 +0200

    DONE

--- dnd-character.ts
+++ dnd-character.ts
@@ -1,9 +1,37 @@
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
-  }
+  hitpoints: number;
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
 
-  public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+constructor(){
+  this.charisma =  DnDCharacter.generateAbilityScore();
+  this.strength =  DnDCharacter.generateAbilityScore();
+  this.dexterity =  DnDCharacter.generateAbilityScore();
+  this.wisdom =  DnDCharacter.generateAbilityScore();
+  this.intelligence =  DnDCharacter.generateAbilityScore();
+  this.constitution =  DnDCharacter.generateAbilityScore();
+  this.hitpoints =  10 + DnDCharacter.getModifierFor(this.constitution);
+}
+
+public static generateAbilityScore(): number {
+  var abilityScore: number = 0;
+  var minDice: number = 6;
+  for (let i = 0; i < 4; i++) {
+    var dice: number = Math.floor(Math.random() * 6) + 1
+    abilityScore += dice;
+    if(dice<minDice){
+      minDice=dice;
+    }
   }
+  return abilityScore-minDice;
+}
+
+public static getModifierFor(abilityValue: number): number {
+  var modifier: number = Math.floor((abilityValue - 10)/2);
+  return modifier;
+}
 }
